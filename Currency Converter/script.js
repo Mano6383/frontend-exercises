7c33f28373ed0e62b03a947b549d2efbf8d788eb
Mano T <manothangarajk@gmail.com>
2025-09-17 19:03:00 +0530

    Update script.js

--- Currency Converter/script.js
+++ Currency Converter/script.js
@@ -1,36 +1,28 @@
 fetch('https://api.frankfurter.dev/v1/currencies')
-.then(res => res.json())
-.then(data => {
-    const leftSelect = document.querySelector('.left_con');
-    const rightSelect = document.querySelector('.right_con');
-
-    for (const [code, name] of Object.entries(data)) {
-        const option1 = document.createElement('option');
-        option1.value = code;
-        option1.textContent = `${code} - ${name}`;
-        leftSelect.appendChild(option1);
-
-        const option2 = document.createElement('option');
-        option2.value = code;
-        option2.textContent = `${code} - ${name}`;
-        rightSelect.appendChild(option2);
-    }
-});
-
-let btn = document.querySelector('.btn');
-btn.addEventListener('click', () => {
-    const leftSelect = document.querySelector('.left_con').value;
-    const rightSelect = document.querySelector('.right_con').value;
-    const leftAmount = document.querySelector('.left_amount').value;
-
-    if (!leftAmount || Number(leftAmount) <= 0) {
-        alert('Please enter a valid amount');
-    } else {
-        fetch(`https://api.frankfurter.dev/v1/latest?amount=${leftAmount}&from=${leftSelect}&to=${rightSelect}`)
-        .then(res => res.json())
-        .then(data => {
-            const rate = Object.values(data.rates)[0];
-            document.querySelector('.right_amount').value = rate;
-        });
-    }
+	.then(response=>response.json())
+	.then(data=>{
+		let currency1=document.querySelector('.left_con')
+		let currency2=document.querySelector('.right_con')
+		for(let [code,name] of Object.entries(data)){
+			let opt=document.createElement('option')
+			opt.value=code
+			opt.innerHTML=`${code}-${name}`
+			currency1.appendChild(opt)
+			currency2.appendChild(opt.cloneNode(true))
+		}
+	})
+let btn=document.querySelector('.btn')
+btn.addEventListener('click',()=>{
+	let currency1=document.querySelector('.left_con').value
+	let currency2=document.querySelector('.right_con').value
+	let amount=document.querySelector('.left_amount').value
+	if(amount==0||amount=="")
+		alert("Enter a valid amount to convert")
+	else {
+    fetch(`https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${currency1}&to=${currency2}`)
+      .then(response => response.json())
+      .then(data => {
+        document.querySelector('.right_amount').value = data.rates[currency2];
+      });
+  }
 });
